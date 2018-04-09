8f3fd5d94789823b6c5a06e529cef5108fb3d53b
ivan minutillo <minutillo.ivan@gmail.com>
2018-04-09 12:12:15 +0200

    improved new UI on apptemplate and sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import style from './style.css'
-import {User, Preferites, Settings, Power, More, Home, Work, Validate} from '../../icons'
+import {User, Preferites, Settings, Power, More} from '../../icons'
 import {NavLink, withRouter} from 'react-router-dom'
 
 const Sidebar = (props) => {
-  // let result = props.agents.reduce(function (r, a) {
-  //   let type = a.relationship.label.split(' ', 3)[1]
-  //   r[type] = r[type] || []
-  //   r[type].push(a)
-  //   return r
-  // }, Object.create(null))
   return (
     <div>
       <div className={style.sidebar_mobile + ' ' + style.sidebar_mobile_active}>
@@ -40,63 +34,63 @@ const Sidebar = (props) => {
           </li>
         </ul>
       </div>
-    <div className={style.sidebar}>
-      <header className={style.sidebar_header}>
-      <div className={style.header_menu}>
-        <div className={style.header_profile}>
-          <NavLink to={'/work/agent/' + props.data.id}>
-            <div className={style.data_image}>
-              <img src={props.data.image} />
-            </div>
-            <h3>{props.data.name}</h3>
-          </NavLink>
-        </div>
-        <div className={style.right_profile}>
-          {/* <span><Bell width={'16'} height={'16'} color='#fff' /></span> */}
-          <span onClick={props.handleToggleProfilePanel}><More onClick={props.handleToggleProfilePanel} color='#fff' width='18' height='18' /></span>
-        </div>
-        <div className={props.profile ? style.right_profile_panel + ' ' + style.profileActive : style.right_profile_panel }>
-          <div className={style.panel_section}>
-            <NavLink to={'/'}>Profile</NavLink>
-            <NavLink to={'/settings'}>Settings</NavLink>
+      <div className={style.sidebar}>
+        <header className={style.sidebar_header}>
+          <div className={style.header_left}>
+            <span className={style.logo} />
+            <ul className={style.section_list}>
+              <li className={style.list_item}><NavLink exact activeClassName={style.active} to={'/'}>Dashboard</NavLink></li>
+              <li className={style.list_item}><NavLink activeClassName={style.active} to={'/work'}>Work</NavLink></li>
+              <li className={style.list_item}><NavLink activeClassName={style.active} to={'/validate'}>Validate</NavLink></li>
+              <li className={style.list_item}><NavLink activeClassName={style.active} to={'/network'}>Network</NavLink></li>
+              <li className={style.list_item}><NavLink activeClassName={style.active} to={'/inventory'}>Inventory</NavLink></li>
+              <li><a href='https://board.net/p/Fair_Health_Care_v4' target='blank'>Welfare</a></li>
+              <li><a target='blank' href="https://wallet.bankofthecommons.coop">Wallet</a></li>
+            </ul>
           </div>
-          <button onClick={props.logout} className={style.logout}>Logout</button>
-        </div>
-        </div>
-      </header>
-      <section className={style.sidebar_section}>
-        <h3 className={style.section_title}>Apps</h3>
-        <ul className={style.section_list}>
-          <li className={style.list_item}><NavLink exact activeClassName={style.active} to={'/'}><span><Home width='18' height='17' color='#fff' /></span> Dashboard</NavLink></li>
-          <li className={style.list_item}><NavLink activeClassName={style.active} to={'/work'}><span><Work width='18' height='17' color='#fff' /></span>Work</NavLink></li>
-          <li className={style.list_item}><NavLink activeClassName={style.active} to={'/validate'}><span><Validate width='18' height='17' color='#fff' /></span>Validate</NavLink></li>
-          <li><a href="https://board.net/p/Fair_Health_Care_v4" target='blank'>Health</a></li>
-          <li><a target='blank' href="https://wallet.bankofthecommons.coop">Wallet</a></li>
-        </ul>
-      </section>
-      {/* <section className={style.aside_section}>
-      <h3 className={style.section_title}>Network</h3>
-      {Object.keys(result).map((item, i) => (
-        <div key={i}>
-          <ul className={style.section_list}>
-            {result[Object.keys(result)[i]].map((agent, i) => (
-              <NavLink key={i} activeClassName={style.activeLink} to={'/agent/' + agent.object.id}>
-                <li>
-                  <div className={style.list_agent}>
-                    <img className={style.agent_photo} src="https://picsum.photos/200/300" />
-                    <div className={style.agent_label}>
-                      <h4>{agent.object.name}</h4>
-                      <h6>{agent.relationship.label.split(' ', 3)[1]}</h6>
-                    </div>
-                  </div>
-                </li>
+          <div className={style.header_menu}>
+            <ul className={style.menu_item}>
+              <li>
+                <a target='blank' href='https://www.opencoopecosystem.net'>Docs</a>
+              </li>
+              <li>
+                <a href='http://fairplayground.info/datasources/editor.php' target='blank' >Stats</a>
+              </li>
+              <li>
+                <a href='https://github.com/opencoopecosystem' target='blank'>Github</a>
+              </li>
+            </ul>
+            <div className={style.header_profile}>
+              <NavLink to={'/agent/' + props.data.id}>
+                <div className={style.data_image}>
+                  <img src={props.data.image} />
+                </div>
+                <h3>{props.data.name}</h3>
               </NavLink>
-            ))}
+            </div>
+            <div className={style.right_profile}>
+              {/* <span><Bell width={'16'} height={'16'} color='#fff' /></span> */}
+              <span onClick={props.handleToggleProfilePanel}><More onClick={props.handleToggleProfilePanel} color='#222' width='18' height='18' /></span>
+            </div>
+            <div className={props.profile ? style.right_profile_panel + ' ' + style.profileActive : style.right_profile_panel }>
+              <div className={style.panel_section}>
+                <NavLink to={'/'}>Profile</NavLink>
+                <NavLink to={'/settings'}>Settings</NavLink>
+              </div>
+              <button onClick={props.logout} className={style.logout}>Logout</button>
+            </div>
+          </div>
+        </header>
+        {/* <section className={style.sidebar_section}>
+          <ul className={style.section_list}>
+            <li className={style.list_item}><NavLink exact activeClassName={style.active} to={'/'}>Dashboard</NavLink></li>
+            <li className={style.list_item}><NavLink activeClassName={style.active} to={'/work'}>Work</NavLink></li>
+            <li className={style.list_item}><NavLink activeClassName={style.active} to={'/validate'}>Validate</NavLink></li>
+            <li><a href="https://board.net/p/Fair_Health_Care_v4" target='blank'>Health</a></li>
+            <li><a target='blank' href="https://wallet.bankofthecommons.coop">Wallet</a></li>
           </ul>
-        </div>
-      ))}
-      </section> */}
-    </div>
+        </section> */}
+      </div>
     </div>
   )
 }
--- src/templates/AppTemplate/index.js
+++ src/templates/AppTemplate/index.js
@@ -1,46 +1,13 @@
 import * as React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
-import AuthenticatedOnly from '../../AuthenticatedOnly'
-import Login from '../../login/'
 import style from './style.css'
 import { 
-  BrowserRouter as Router,
   Route,
-  Link,
   Redirect,
   withRouter
  } from 'react-router'
-import Flag from '../../components/flag'
 import Sidebar from '../../components/sidebar'
-
-
-// const PrivateRoute = ({ component: Component, ...rest }) => (
-//   <Route
-//     {...rest}
-//     render={props =>
-//       fakeAuth.isAuthenticated ? (
-//         <Component {...props} />
-//       ) : (
-//         <Redirect
-//           to={{
-//             pathname: "/login",
-//             state: { from: props.location }
-//           }}
-//         />
-//       )
-//     }
-//   />
-// );
-
-
-// class AuthenticatedOnly extends React.Component {
-//   render () {
-//     let isLoggedIn = localStorage.getItem('oce_token')
-//     return (isLoggedIn ? this.props.children : this.props.unauthenticatedComponent)
-//   }
-// }
-
 
 const AppTemplate = ({component: Component, ...rest}) => {
   let isLoggedIn = localStorage.getItem('oce_token')
@@ -52,10 +19,9 @@ const AppTemplate = ({component: Component, ...rest}) => {
           rest.loading ? <strong>Loading...</strong> : (
             rest.error ? <p style={{ color: '#F00' }}>API error</p> : (
               <div >
-                <Flag />
                 <Sidebar data={rest.data} agents={rest.data.agentRelationships} />
                 <div className={style.container}>
-                  <Component {...props} />
+                  <Component {...props} agentData={rest} />
                 </div>
               </div>
             )
@@ -72,28 +38,6 @@ const AppTemplate = ({component: Component, ...rest}) => {
     />
   )
 }
-
-// class AppTemplate extends React.Component {
-//   render () {
-//     const {data, loading, error, children} = this.props
-//     console.log('ciao')
-//     console.log(error)
-//     return (
-//       <AuthenticatedOnly unauthenticatedComponent={<Login />}>
-//         {loading ? <strong>Loading...</strong> : (
-//           error ? <p style={{ color: '#F00' }}>API error</p> : (
-//             <div >
-//               <Flag />
-//               <Sidebar data={data} agents={data.agentRelationships} />
-//               <div className={style.container}>
-//                 {children}
-//               </div>
-//             </div>
-//           ))}
-//       </AuthenticatedOnly>
-//     )
-//   }
-// }
 
 const agentPlans = gql`
 query ($token: String) {
