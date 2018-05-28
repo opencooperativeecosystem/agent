34a193c975fe1fd0700c356709e8779f387a1709
ivan minutillo <minutillo.ivan@gmail.com>
2018-05-28 17:56:21 +0200

    scaffolding new commitment portal

---
+++ src/components/newCommitmentModal/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Modal = () => (
+    <h1>ecco</h1>
+)
+
+export default Modal
---
+++ src/components/newCommitmentModal/wrapper.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import CardModal from './index'
+import Modal from 'react-modal'
+import style from './style.css'
+
+const customStyles = {
+  overlay: {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(0, 0, 0, 0.6)',
+    zIndex            : 9999999,
+    height            : '100%',
+    justifyContent    : 'center',
+    overflow          : 'auto'
+  }
+  // content: {
+  //   width                 : '450px',
+  //   boxShadow             : '0 2px 8px 3px rgba(0,0,0,.3)',
+  //   zIndex                : 9999999999,
+  //   backgroundColor       : '#EDEFF0',
+  //   padding:  0,
+  //   margin:  '40px auto',
+  //   position: 'relative'
+  // }
+}
+
+const UModal = ({modalIsOpen, closeModal}) => (
+  <Modal
+    isOpen={modalIsOpen}
+    onRequestClose={closeModal}
+    contentLabel='Commitment Modal'
+    style={customStyles}
+    className={{
+      base: style.cardModal
+    }}
+    >
+      <CardModal  />
+  </Modal>
+)
+
+export default UModal
