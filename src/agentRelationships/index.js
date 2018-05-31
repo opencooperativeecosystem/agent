790e8b0f2ae2fd02a5528a37051327a9b115e0bd
ivan minutillo <minutillo.ivan@gmail.com>
2018-05-31 18:23:55 +0200

    modularized agentRelationships panel into a stateless component

---
+++ src/agentRelationships/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Icons, Panel, NavigationItem } from "oce-components/build";
+import style from "./style.css";
+import { NavLink } from "react-router-dom";
+import {PropsRoute} from '../helpers/router'
+import AgentPlans from '../agentPlans/wrapper'
+
+const AgentRelationships = props => (
+    <div className={style.container}>
+    <Panel icon={<Icons.Globe width="18" color="#f0f0f0" />} title="Network">
+      <div>
+        {props.relationships.map((item, i) => (
+          <div className={style.container_link} key={i}>
+            <NavLink
+              activeClassName={style.active}
+              to={`${props.match.path}/agent/` + item.object.id}
+            >
+              <NavigationItem img={item.object.image} title={item.object.name} />
+            </NavLink>
+          </div>
+        ))}
+      </div>
+    </Panel>
+    <PropsRoute path={`${props.match.path}/agent/:id`} component={AgentPlans} />
+    </div>
+);
+
+export default AgentRelationships;
