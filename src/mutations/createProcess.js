ba768a9402bda9e06150d140579871355821caeb
ivan minutillo <minutillo.ivan@gmail.com>
2018-05-31 18:22:59 +0200

    updated createProcess mutation

--- src/mutations/createProcess.js
+++ src/mutations/createProcess.js
@@ -1,13 +1,14 @@
 import gql from 'graphql-tag'
 
 const createProcess = gql`
-mutation ($token: String!, $name: String!, $planned: String!, $duration: Int!, $scope: Int!, $note: String ) {
+mutation ($token: String!, $planId: Int! $name: String!, $planned: String!, $duration: Int!, $scope: Int!, $note: String ) {
     createProcess(
         token: $token,
         name: $name,
         plannedStart: $planned,
         plannedDuration: $duration,
         scopeId: $scope,
+        planId: $planId,
         note: $note) {
       process {
         id
