f0f12368470a02670289f5f10fbe9d8ca70b2897
ivan minutillo <minutillo.ivan@gmail.com>
2018-06-14 16:39:17 +0200

    deleted tags to define process status based on the first process

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -5,24 +5,20 @@ import moment from 'moment'
 import {withRouter} from 'react-router-dom'
 const Cards = ({data, link, match, location}) => {
   let newData = data.map(x => ({...x, date: moment(x.plannedOn).unix()}))
-  console.log(link)
-  console.log(match)
-  console.log(location)
   return (
     <div className={style.section_wrapper}>
       {newData
       .sort((a, b) => b.date - a.date)
       .map((org, i) => (
-      org.planProcesses[0].isFinished ? ''
-      : <div key={i} className={style.lists_item}>
+        <div key={i} className={style.lists_item}>
           <Link key={i} to={link + '/' + org.id} className={style.link}>
             <h4 className={style.item_title}>{org.name.length > 0 ? org.name : org.planProcesses[0].name}</h4>
             <h5 className={style.plan_scope}>{org.note}</h5>
             <div className={style.item_info}>
               <span className={style.info_date}>From {moment.utc(org.plannedOn).format('DD MMM YYYY')} to {moment.utc(org.due).format('DD MMM YYYY')}</span>
-              <span className={org.planProcesses[0].isFinished ? style.info_status + ' ' + style.completed : org.planProcesses[0].isStarted ? style.info_status + ' ' + style.wip : style.info_status + ' ' + style.todo}>
+              {/* <span className={org.planProcesses[0].isFinished ? style.info_status + ' ' + style.completed : org.planProcesses[0].isStarted ? style.info_status + ' ' + style.wip : style.info_status + ' ' + style.todo}>
                 {org.planProcesses[0].isFinished ? 'Completed' : org.planProcesses[0].isStarted ? 'In Progress' : 'To do' }
-              </span>
+              </span> */}
             </div>
           </Link>
       </div>
