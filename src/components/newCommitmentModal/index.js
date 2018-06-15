32c2ea5f65116a04a005b56f7898177d4a274b6a
ivan minutillo <minutillo.ivan@gmail.com>
2018-06-15 09:35:11 +0200

    controls and form schema on create commitment

--- src/components/newCommitmentModal/index.js
+++ src/components/newCommitmentModal/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { NewCommitmentModal } from "oce-components/build";
+import { NewCommitmentModal, SupTitle, Wrapper, Input, Textarea, Button, Select, Icons } from "oce-components/build";
 import { compose, withState, withHandlers } from "recompose";
 import moment from "moment";
-import { graphql, withApollo, ApolloConsumer } from "react-apollo";
+import { graphql, withApollo } from "react-apollo";
 import getResourcesQuery from "../../queries/getResources";
 import getUnitsQuery from "../../queries/getUnits";
 import DatePicker from "react-datepicker";
@@ -10,6 +10,9 @@ import style from './style.css'
 import Plan from "../../queries/getPlan";
 import CreateCommitment from '../../mutations/CreateCommitment'
 import { ToastContainer, toast } from 'react-toastify';
+import {Form, withFormik, Field} from 'formik' 
+import * as Yup from 'yup'
+import Alert from '../alert'
 import 'react-toastify/dist/ReactToastify.css';
 require("react-datepicker/dist/react-datepicker-cssmodules.css");
 
@@ -36,22 +39,22 @@ options.unshift(
 );
 
 const NewCommModal = ({
-  createCommitment,
-  units,
-  date,
-  resources,
-  handleNote,
-  handleDate,
   match,
   scopeId,
   planId,
   processId,
-  handleEvent,
-  handleQty,
-  handleResource,
-  handleUnit,
   modalIsOpen,
-  toggleNewCommitmentModal
+  toggleNewCommitmentModal,
+  handleEvent,
+  handleSubmit,
+  setFieldValue,
+  setFieldTouched,
+  errors,
+  touched,
+  values,
+  client,
+  resources,
+  units
 }) => {
   let resourcesOptions = resources.map((ev, i) => (
     <option key={i} value={ev.id}>
@@ -74,45 +77,168 @@ const NewCommModal = ({
     </option>
   );
   return (
-    <ApolloConsumer>
-      {client => (
-        <NewCommitmentModal
-          createCommitment={() => createCommitment(client, planId, processId, match, scopeId)}
-          events={options}
-          units={unitsOptions}
-          resources={resourcesOptions}
-          handleEvent={ev => handleEvent(client, ev)}
-          handleResource={handleResource}
-          handleQty={handleQty}
-          handleUnit={handleUnit}
-          handleDate={handleDate}
-          dateComponent={
-            <DatePicker
-              selected={date}
-              onChange={handleDate}
-              dateFormatCalendar={"DD MMM YYYY"}
-              className={style.dateComp}
-              withPortal
-            />}
-          handleNote={handleNote}
+    <Form>
+    <Wrapper>
+        <SupTitle dark icon={<Icons.Text widht='18' height='18' color='#383C39' />} text='New commitment' />
+        <div className={style.commitmentWrapper}>
+            <div className={style.commInput}>
+              <EventSelect 
+                value={values.event}
+                onChange={setFieldValue}
+                error={errors.start}
+                touched={touched.start}
+                handleEvent={handleEvent}
+                options={options}
+                client={client}
+              />
+              {errors.event && touched.event && <Alert>{errors.event}</Alert>}
+            </div>
+            <div className={style.commInput}>
+            <Field
+              name="resource"
+              render={({ field /* _form */ }) => (
+                <Select name={field.name} onChange={field.onChange}>{resourcesOptions.map((opt) => opt)}</Select>
+              )}
+            />
+            {errors.resource && touched.resource && <Alert>{errors.resource}</Alert>}
+            </div>
+            <div className={style.commInput}>
+              <Field
+                name="qty"
+                render={({ field /* _form */ }) => (
+                  <Input name={field.name} onChange={field.onChange} placeholder='00.00' />
+                )}
+              />
+              {errors.qty && touched.qty && <Alert>{errors.qty}</Alert>}
+            </div>
+            <div className={style.commInput}>
+              <Field
+                name="unit"
+                render={({ field /* _form */ }) => (
+                  <Select name={field.name} onChange={field.onChange}>{unitsOptions.map((opt) => opt)}</Select>
+                )}
+              />
+              {errors.unit && touched.unit && <Alert>{errors.unit}</Alert>}
+            </div>
+        </div>
+        <div>
+            <StartDate 
+              value={values.date}
+              onChange={setFieldValue}
+              onBlur={setFieldTouched}
+              error={errors.start}
+              touched={touched.start}
+            />
+        </div>
+        <div style={{height: '150px', marginTop: '8px', marginBottom: '8px'}}>
+        <Field
+          name="note"
+          render={({ field /* _form */ }) => (
+            <Textarea name={field.name} onChange={field.onChange} placeholder='Write some note' />
+          )}
         />
-      )}
-      </ApolloConsumer>
+        </div>
+        <div>
+            <Button>Publish</Button>
+        </div>
+    </Wrapper>
+    </Form>
+     
   );
 };
 
+
+const EventSelect = (props) => {
+  const handleChange = value => {
+    props.onChange('event', value.target.value)
+    props.handleEvent(props.client, value)
+  }
+  return (
+    <div>
+      <Select name={'event'} onChange={handleChange}>{props.options.map((opt) => opt)}</Select>
+      {props.error && props.touched && <Alert>{props.error}</Alert>}
+    </div>
+  )
+}
+
+const StartDate = (props) => {
+  const handleChange = value => {
+    props.onChange('date', value);
+  };
+  return (
+    <div className={style.dateWrapper}>
+      <h5 className={style.dateName}>Start</h5>
+      <DatePicker
+      selected={props.value}
+      onChange={handleChange}
+      dateFormatCalendar={'DD MMM YYYY'}
+      withPortal
+    />
+    {props.error && props.touched && <Alert>{props.error}</Alert>}
+    </div>
+  )
+}
+
+
+
+
 export default compose(
-  withState("event", "onEvent", null),
-  withState("resource", "onResource", null),
-  withState("qty", "onQty", null),
-  withState("unit", "onUnit", null),
-  withState("note", "onNote", null),
-  withState("date", "onDate", moment()),
   withState("resources", "onResourcesArray", []),
   withState("units", "onUnitsArray", []),
+  withFormik({
+    mapPropsToValues: () => ({ event: '', note: '', resource: '', qty: '', unit: '', date: moment() }),
+    validationSchema: Yup.object().shape({
+      event: Yup.string().required(),
+      note: Yup.string(),
+      resource: Yup.string().required(),
+      qty: Yup.number().required(),
+      unit: Yup.string().required(),
+      date: Yup.string()
+    }),
+    handleSubmit: (values, {props, resetForm, setErrors, setSubmitting}) => {
+      let date = moment(props.date).format("YYYY-MM-DD")
+      return props.client.mutate({
+          mutation: CreateCommitment,
+          variables: {
+              token: localStorage.getItem("oce_token"),
+              action: values.event.toLowerCase(),
+              due: date,
+              note: values.note,
+              committedResourceClassifiedAsId: values.resource,
+              committedUnitId: values.unit,
+              committedNumericValue: values.qty,
+              inputOfId: props.processId,
+              outputOfId: props.processId,
+              planId: props.planId,
+              scopeId: props.scopeId
+          },
+          update: (cache, {data: {createCommitment}}) => {
+            const data = cache.readQuery({query: Plan, variables: {
+              token: localStorage.getItem('oce_token'),
+              planId: Number(props.planId)
+          }})
+          const processIndex = data.viewer.plan.planProcesses.findIndex(process => Number(process.id) === Number(props.processId))
+          if (createCommitment.commitment.action === 'work') {
+              data.viewer.plan.planProcesses[processIndex].committedInputs.push(createCommitment.commitment)
+            } else {
+                data.viewer.plan.planProcesses[processIndex].committedOutputs.push(createCommitment.commitment)
+          }
+            cache.writeQuery({
+                query: Plan,
+                data,
+                variables: {
+                  token: localStorage.getItem("oce_token"),
+                  planId: Number(props.match.params.id)
+                }
+            })
+        }
+      })
+      .then(res => () => toast("Wow so easy !"))
+      .catch(e => () => toast("error !"))
+    }
+  }),
   withHandlers({
     handleEvent: props => (client, ev) => {
-      props.onEvent(ev.target.value);
       return client
         .query({
           query: getResourcesQuery,
@@ -144,61 +270,5 @@ export default compose(
         })
         .catch(e => console.log(e));
     },
-    handleResource: props => event => {
-      props.onResource(event.target.value);
-    },
-    handleQty: props => event => {
-      props.onQty(event.target.value);
-    },
-    handleUnit: props => event => {
-      props.onUnit(event.target.value);
-    },
-    handleDate: props => event => {
-      props.onDate(event);
-    },
-    handleNote: props => event => {
-      props.onNote(event.target.value);
-    },
-    createCommitment: props => (client, planId, processId, match, scopeId) => {
-      let date = moment(props.date).format("YYYY-MM-DD")
-      return client.mutate({
-          mutation: CreateCommitment,
-          variables: {
-              token: localStorage.getItem("oce_token"),
-              action: props.event.toLowerCase(),
-              due: date,
-              note: props.note,
-              committedResourceClassifiedAsId: props.resource,
-              committedUnitId: props.unit,
-              committedNumericValue: props.qty,
-              inputOfId: processId,
-              outputOfId: processId,
-              planId: planId,
-              scopeId: scopeId
-          },
-          update: (cache, {data: {createCommitment}}) => {
-            const data = cache.readQuery({query: Plan, variables: {
-              token: localStorage.getItem('oce_token'),
-              planId: Number(planId)
-          }})
-          const processIndex = data.viewer.plan.planProcesses.findIndex(process => Number(process.id) === Number(props.processId))
-          if (createCommitment.commitment.action === 'work') {
-              data.viewer.plan.planProcesses[processIndex].committedInputs.push(createCommitment.commitment)
-            } else {
-                data.viewer.plan.planProcesses[processIndex].committedOutputs.push(createCommitment.commitment)
-          }
-            cache.writeQuery({
-                query: Plan,
-                data,
-                variables: {
-                  token: localStorage.getItem("oce_token"),
-                  planId: Number(match.params.id)
-                }
-            })
-        }
-      })
-      .then(res => () => toast("Wow so easy !"))
-      .catch(e => () => toast("error !"))
-    }
   })
 )(NewCommModal);
--- src/components/newCommitmentModal/wrapper.js
+++ src/components/newCommitmentModal/wrapper.js
@@ -2,18 +2,24 @@ import React from 'react'
 import CardModal from './index'
 import Modal from '../modal'
 import style from './style.css'
+import {  ApolloConsumer } from "react-apollo";
 
 const NCModal = ({toggleNewCommitmentModal, modalIsOpen, closeModal, planId, match, scopeId, processId}) => (
   <Modal
     isOpen={modalIsOpen}
     toggleModal={toggleNewCommitmentModal}
     >
+        <ApolloConsumer>
+       {client => (
       <CardModal
         processId={processId}
         planId={planId}
         match={match}
         scopeId={scopeId}
+        client={client}
       />
+    )}
+    </ApolloConsumer>
   </Modal>
 )
 
