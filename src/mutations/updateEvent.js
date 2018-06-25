f9e85309890a311783c6d1f674d37eaec8ac905b
ivan minutillo <minutillo.ivan@gmail.com>
2018-06-25 21:05:43 +0200

    updateEvent mutations

---
+++ src/mutations/updateEvent.js
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag'
+
+export default gql`
+mutation ($token: String!, $id: Int!, $start: String!, $requestDistribution: Boolean, $scopeId: Int!, $note: String, $affectedNumericValue: String!, $affectedUnitId: Int! ) {
+  updateEconomicEvent(
+    token: $token,
+    id: $id,
+    start: $start,
+    scopeId: $scopeId, 
+    requestDistribution: $requestDistribution, 
+    note: $note,
+    affectedNumericValue: $affectedNumericValue,
+    affectedUnitId: $affectedUnitId,
+    ) {
+      economicEvent {
+        action
+        note
+        start
+        id
+        requestDistribution
+        scope {
+          id
+        }
+        provider {
+          name
+          id
+          image
+        }
+        affectedQuantity {
+          unit {
+            name
+          }
+          numericValue
+        }
+      }
+  }
+}`
