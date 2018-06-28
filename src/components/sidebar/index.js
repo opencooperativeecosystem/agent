72038ab15886bcefcbb6bf6450778fe20aaaf3b2
ivan minutillo <minutillo.ivan@gmail.com>
2018-06-28 15:59:28 +0200

    plan creation directly from sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,11 +1,56 @@
 import {compose, withHandlers} from 'recompose'
 import Component from './sidebar'
+import moment from "moment";
+import {withFormik} from 'formik'
+import * as Yup from 'yup'
+import createPlan from '../../mutations/CreatePlan'
+import updateNotification from "../../mutations/updateNotification";
+import {Icons} from 'oce-components/build'
+import { graphql } from "react-apollo";
+import React from 'react'
 
 export default compose(
-    withHandlers({
-      logout: props => event => {
-        localStorage.removeItem('oce_token')
-        window.location.reload()
-      }
-    })
-  )(Component)
+  graphql(createPlan, { name: "createPlanMutation" }),
+  graphql(updateNotification, {name: 'updateNotification'}),
+  withFormik({
+    mapPropsToValues: (props) => ({ name: '', note: '', start: moment(), due: moment() }),
+    validationSchema: Yup.object().shape({
+       name: Yup.string().required(),
+       note: Yup.string(),
+       start: Yup.string().required(),
+       due: Yup.string().required()
+    }),
+    handleSubmit: (values, {props}) => {
+      let date = moment(values.due).format("YYYY-MM-DD");
+      props
+        .createPlanMutation({
+          variables: {
+            token: localStorage.getItem("oce_token"),
+            name: values.name,
+            due: date,
+            note: values.note
+          }
+        })
+        .then(data => {
+          props.updateNotification({variables: {
+            message: <div style={{fontSize:'14px'}}><span style={{marginRight: '10px', verticalAlign: 'sub'}}><Icons.Bell width='18' height='18' color='white' /></span>Plan {data.data.createPlan.plan.name} created successfully!</div>,
+            type: 'success'
+          }})
+          return props.history.push(`canvas/${data.data.createPlan.plan.id}`);
+        }, (e) => {
+          const errors = e.graphQLErrors.map(error => error.message)
+          props.updateNotification({variables: {
+            message: <div style={{fontSize:'14px'}}><span style={{marginRight: '10px', verticalAlign: 'sub'}}><Icons.Cross width='18' height='18' color='white' /></span>{errors}</div>,
+            type: 'alert'
+          }})
+          props.setSubmitting(false)
+       })
+    }
+  }),
+  withHandlers({
+    logout: props => event => {
+      localStorage.removeItem('oce_token')
+      window.location.reload()
+    }
+  })
+)(Component)
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,8 +1,45 @@
 import React from 'react'
 import style from './style.css'
-import {Icons} from 'oce-components/build'
+import {Icons, Input, Textarea, Button} from 'oce-components/build'
+import DatePicker from 'react-datepicker'
+import {Form, Field} from 'formik'
+import Alert from '../alert'
+import {NavLink} from 'react-router-dom'
+require("react-datepicker/dist/react-datepicker-cssmodules.css")
 
-import {NavLink, withRouter} from 'react-router-dom'
+const StartDate = (props) => {
+  const handleChange = value => {
+    props.onChange('start', value);
+  };
+  return (
+    <div>
+      <DatePicker
+      selected={props.value}
+      onChange={handleChange}
+      dateFormatCalendar={'DD MMM YYYY'}
+      withPortal
+    />
+    {props.error && props.touched && <Alert>{props.error}</Alert>}
+    </div>
+  )
+}
+
+const DueDate = (props) => {
+  const handleChange = value => {
+    props.onChange('due', value);
+  };
+  return (
+    <div>
+    <DatePicker
+      selected={props.value}
+      onChange={handleChange}
+      dateFormatCalendar={'DD MMM YYYY'}
+      withPortal
+    />
+    {props.error && props.touched && <Alert>{props.error}</Alert>}
+    </div>
+  )
+}
 
 const Sidebar = (props) => {
   return (
@@ -23,14 +60,53 @@ const Sidebar = (props) => {
             <h3>{props.data.name}</h3>
           </NavLink>
         </div>
-        <ul className={style.section_list}>
+        <div className={style.section_planCreation}>
+        <h4 className={style.planCreation_title}>Create a new plan</h4>
+        <Form>
+          <div className={style.form_input}>
+            <Field name="name" render={({ field /* _form */ }) => (<Input type={'dark'} {...field} placeholder='Type the plan name' />)} />
+            {props.errors.name && props.touched.name && <Alert>{props.errors.name}</Alert>}
+          </div>
+          <div className={style.formPlanWrapper}>
+          <div className={style.form_note}>
+            <Field name="note" render={({ field /* _form */ }) => (<Textarea {...field} placeholder='What is the plan about' />)} />
+          </div>
+          <div className={style.dates}>
+            <div className={style.dateWrapper}>
+              <h5 className={style.dateName}><span style={{verticalAlign: 'sub'}}><Icons.Calendar width='16' height='16' color='#707BA0' /></span> Start</h5>
+              <StartDate
+                value={props.values.start}
+                onChange={props.setFieldValue}
+                onBlur={props.setFieldTouched}
+                error={props.errors.start}
+                touched={props.touched.start}
+              />
+            </div>
+            <div className={style.dateWrapper}>
+              <h5 className={style.dateName}><span style={{verticalAlign: 'sub'}}><Icons.Calendar width='16' height='16' color='#707BA0' /></span> Due</h5>
+              <DueDate
+                value={props.values.due}
+                onChange={props.setFieldValue}
+                onBlur={props.setFieldTouched}
+                error={props.errors.due}
+                touched={props.touched.due}
+              />
+            </div>
+          </div>
+          </div>
+          <div className={style.form_button}>
+            <Button>Create Plan</Button>
+          </div>
+          </Form>
+        </div>
+        {/* <ul className={style.section_list}>
           <li className={style.list_item}><NavLink exact activeClassName={style.active} to={'/'}>Dashboard</NavLink></li>
           <li className={style.list_item}><NavLink activeClassName={style.active} to={'/plans'}>Plans</NavLink></li>
           <li className={style.list_item}><NavLink activeClassName={style.active} to={'/wallet'}>Wallet</NavLink></li>
           {/* <li className={style.list_item}><NavLink activeClassName={style.active} to={'/network'}>Network</NavLink></li> */}
-          <li className={style.list_item}><a href='http://fairplayground.info/datasources/editor.php' target='blank' >Stats</a></li>
+          {/* <li className={style.list_item}><a href='http://fairplayground.info/datasources/editor.php' target='blank' >Stats</a></li>
           <li><a href='https://board.net/p/Fair_Health_Care_v4' target='blank'>Welfare</a></li>
-        </ul>
+        </ul> */}
       </div>
     </div>
     : <header className={style.sidebar_menu + ' ' + style.sidebar_menu_collapsed}>
@@ -39,4 +115,4 @@ const Sidebar = (props) => {
   )
 }
 
-export default withRouter(Sidebar)
+export default Sidebar
