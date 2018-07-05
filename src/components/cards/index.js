054f5da6a803998809ecbbe8163bb66a6a09cbed
ivan minutillo <minutillo.ivan@gmail.com>
2018-07-05 21:13:07 +0200

    remove startDate on plan card

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -15,7 +15,7 @@ const Cards = ({data, link, match, location}) => {
             <h4 className={style.item_title}>{org.name.length > 0 ? org.name : org.planProcesses[0].name}</h4>
             <h5 className={style.plan_scope}>{org.note}</h5>
             <div className={style.item_info}>
-              <span className={style.info_date}>From {moment.utc(org.plannedOn).format('DD MMM YYYY')} to {moment.utc(org.due).format('DD MMM YYYY')}</span>
+              <span className={style.info_date}>Due {moment(org.due).format('DD MMM YYYY')}</span>
               {/* <span className={org.planProcesses[0].isFinished ? style.info_status + ' ' + style.completed : org.planProcesses[0].isStarted ? style.info_status + ' ' + style.wip : style.info_status + ' ' + style.todo}>
                 {org.planProcesses[0].isFinished ? 'Completed' : org.planProcesses[0].isStarted ? 'In Progress' : 'To do' }
               </span> */}
