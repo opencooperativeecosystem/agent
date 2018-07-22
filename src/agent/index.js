54d635d4b1684ec82a4b35e0892a530c67822217
ivan minutillo <minutillo.ivan@gmail.com>
2018-07-22 12:21:19 +0200

    UI edits on agent page

--- src/agent/index.js
+++ src/agent/index.js
@@ -6,22 +6,25 @@ import Item from '../components/inventoryItem'
 import PanelPlans from './plans'
 import {Icons, Panel, NavigationItem} from 'oce-components/build'
 
-const Agent = ({data}) => {
+const Agent = ({data, toggleModal, isOpen}) => {
   return (
     <section className={style.agent}>
-      <Panel data-testid='diary' icon={<Icons.Diary width='18' color='#f0f0f0' />} title='Diary'>
+      <Panel data-testid='diary' icon={<div className={style.info_image} style={{backgroundImage: data.image ? `url(${data.image})` : `url('./images/sample.png')`}} />} title={data.name}>
         <div className={style.agent_profile}>
           <div className={style.agent_info}>
-            <div className={style.info_image}>
-              <span className={style.image_photo} style={{backgroundImage: data.image ? `url(${data.image})` : `url('./images/sample.png')`}} />
-            </div>
-            <h1 className={style.info_title}>{data.name}</h1>
             {data.email ? <h3 className={style.info_email}>{data.email}</h3> : null}
             {data.note ? <h4 className={style.info_note}>{data.note}</h4> : null}
+            {data.agentSkills.length > 0 ? 
+            <div className={style.info_skills}>
+            <h4 className={style.skills_title}>skills</h4>
+              {data.agentSkills.slice(0,3).map((skill, i) => (
+                <div className={style.skill_item} key={i}>{skill.name}</div>
+              ))}
+              <div onClick={toggleModal} className={style.skill_others}>+{data.agentSkills.slice(3).length}</div>
+            </div>  
+            : null}
           </div>
-          {data.agentEconomicEvents.length > 0
-          ? <Feed feed={data.agentEconomicEvents} />
-          : <div className={style.emptyBox}>No item in this section :(</div>}
+          {data.agentEconomicEvents.length > 0 ? <Feed feed={data.agentEconomicEvents} /> : null }
         </div>
       </Panel>
       <Panel data-testid='network' icon={<Icons.Globe width='18' color='#f0f0f0' />} title={data.type === 'Person' ? 'Network' : 'Participants'}>
@@ -41,7 +44,7 @@ const Agent = ({data}) => {
         </div>
       </Panel>
       <PanelPlans id={data.id} />
-      <Panel data-testid='inventory' icon={<Icons.Inventory width='18' color='#f0f0f0' />} title='Inventory'>
+      {/* <Panel data-testid='inventory' icon={<Icons.Inventory width='18' color='#f0f0f0' />} title='Inventory'>
         <div className={style.resources_list}>
           {data.ownedEconomicResources.length > 0
             ? data.ownedEconomicResources.map((item, i) => (
@@ -50,7 +53,7 @@ const Agent = ({data}) => {
           : <div className={style.emptyBox}>No item in this section :(</div>
           }
         </div>
-      </Panel>
+      </Panel> */}
     </section>
   )
 }
--- src/agent/wrapper.js
+++ src/agent/wrapper.js
@@ -3,22 +3,32 @@ import { graphql } from "react-apollo";
 import Component from "./index";
 import agentQuery from "../queries/getAgent";
 import { LoadingMini } from "../components/loading";
-import {compose} from "recompose";
+import {compose, withState, withHandlers} from "recompose";
+import SkillsModal from "../components/skillsModal";
 
 class AgentWrapper extends React.Component {
   render() {
-    const { loading, error, data} = this.props;
+    const { loading, error, data, modalIsOpen, toggleModal } = this.props;
     return loading ? (
       <LoadingMini />
     ) : error ? (
       <p style={{ color: "#F00" }}>API error</p>
     ) : (
-      <Component data={data} />
+      <div>
+        <Component data={data} isOpen={modalIsOpen} toggleModal={toggleModal} />
+        <SkillsModal isOpen={modalIsOpen} toggleModal={toggleModal} skills={data.agentSkills}/>
+      </div>
     );
   }
 }
 
 export default compose(
+  withState("modalIsOpen", "toggleModalIsOpen", false),
+  withHandlers({
+    toggleModal: props => () => {
+      props.toggleModalIsOpen(!props.modalIsOpen)
+    }
+  }),
   graphql(agentQuery, {
     options: props => ({
       variables: {
