372f130e09a707c400839cacf5c97644714c87cd
ivan minutillo <minutillo.ivan@gmail.com>
2018-07-22 12:20:19 +0200

    queries and mutation related recipes

---
+++ src/queries/getRecipes.js
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag'
+
+export default gql`
+query($token: String) {
+    viewer(token: $token) {
+      allRecipes {
+        id
+        name
+        image
+        category
+        processCategory
+        note
+      }
+    }
+  }
+`
